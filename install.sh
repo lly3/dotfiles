97e424d1cffa353e7db20d879245138cfc7b6c03
melon_arch <momobp114@gmail.com>
2022-02-02 21:59:38 +0000

    dotfiles

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# my install script for pacman on arch linux w/ systemd
+
+# set keyboard layout
+echo "KEYMAP=colemak" >> /etc/vconsole.conf
+
+sudo pacman -Syu
+sudo pacman -S git
+
+# zsh
+sudo pacman -S zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # ohmyzsh
+
+sudo chsh -s /bin/zsh
+
+# fonts
+sudo pacman -S noto-fonts
+sudo pacman -S ttf-anonymous-pro
+sudo pacman -S ttf-nerd-fonts-symbols
+
+# alacritty & picom
+sudo pacman -S alacritty
+sudo pacman -S picom
+
+# make install dwm st dmenu
+sudo pacman -S make
+sudo mv ./{dwm,st,dmenu} /usr/src/
+cd /usr/src/dwm && sudo make clean install
+cd /usr/src/st && sudo make clean install
+cd /usr/src/dmenu && sudo make clean install
+
+mv ./.xinitrc ~/
+
+# set up wallpaper
+sudo pacman -S feh
+mv ./wallpaper/83106977_p0.jpg ~/Pictures
+
---
+++ nvim/init.vim
@@ -0,0 +1,21 @@
+"source $HOME/.config/nvim/vim-plug/plugins.vim
+"source $HOME/.config/nvim/lsp-config.vim
+"
+"luafile $HOME/.config/nvim/lua/plugins/compe-config.lua
+"luafile $HOME/.config/nvim/lua/plugins/lualine.lua
+"luafile $HOME/.config/nvim/lua/lsp/python-ls.lua
+"luafile $HOME/.config/nvim/lua/lsp/clang-ls.lua
+"luafile $HOME/.config/nvim/lua/lsp/asm-ls.lua
+
+:set number relativenumber
+
+" normal mode map
+:nnoremap h k
+:nnoremap k j
+:nnoremap j h
+
+" visual mode map
+:vnoremap h k
+:vnoremap k j
+:vnoremap j h
+
