5f9471fc33c92cf75ace2ff439898d85b93adf0f
oclassiccato <46718155+oclassiccato@users.noreply.github.com>
2022-04-03 16:20:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 echo "KEYMAP=colemak" > /etc/vconsole.conf
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm git zsh noto-fonts ttf-anonymous-pro ttf-nerd-fonts-symbols alacritty picom make feh base-devel neovim python3 python-pip
+sudo pacman -S --noconfirm git zsh noto-fonts ttf-anonymous-pro ttf-nerd-fonts-symbols alacritty picom make feh base-devel neovim python3 python-pip xorg-server xorg-apps xorg-xinit xterm
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # ohmyzsh
